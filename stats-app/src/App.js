748fd5c2d5417f984509e0504715fa3b127adfa8
baseigfr <baseigfr@mtu.edu>
2020-11-19 09:09:26 -0600

    added semantic UI, user, login pages

--- stats-app/src/App.js
+++ stats-app/src/App.js
@@ -1,11 +1,30 @@
-
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+
+
+
+
+
+// one page called Login -> index.js
+// login or sign up functions. 
+// all info in state that user should have
+// if call is successful update state with data
+// pass both functions into js file. both login and register in form
+// if this.signup = false bring up sign up page 
+// 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello Team</h1>
+    
+<div className="App">
+    
+    <Login />
+    <Register />
+    
     </div>
+
+
   );
 }
 
