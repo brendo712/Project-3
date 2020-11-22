a8543ecb0b4230393b60a1601ae22fb5c995fb36
baseigfr <baseigfr@mtu.edu>
2020-11-21 18:22:36 -0600

    added LoginRegisterForm just like instructions. Not sure where to route

---
+++ stats-app/src/NewLobbyForm/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import LobbyList from '../LobbyList';
+import { Form, Button, Label } from 'semantic-ui-react'
+
+
+    export default class NewLobbyForm extends Component {
+
+        constructor(props) {
+          super(props)
+      
+          this.state = {
+            kpm: [],
+            
+          }
+        }
+
+    handleChange = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }    
+
+  
+    componentDidMount() {
+        // get the dogs when this component is first rendered
+        this.createUser()
+      }
+  
+      createUser = async () => {
+      
+        
+        try {
+          const response = await fetch("https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/beau%2311804/battle", {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": "f859fe25ecmsh57af3b8c67c5160p1d7f46jsnef69862545ea",
+                "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com"
+              }
+
+              
+          })
+            const data = await response.json()
+            console.log(data)
+            
+            this.setState({
+              kpm: data.data
+            })
+        
+          }
+          catch(err) {
+            console.log('Error in componentDidMount:')
+            console.log(err)
+          }
+        }
+      
+
+
+
+            // body: JSON.stringify()
+            
+      
+
+    
+  // fun tip: fragments! grouping elements without divs
+  // https://reactjs.org/docs/fragments.html
+    render() {
+      console.log("render() in NewLobbyForm is running")
+      return (
+        <LobbyList
+        kpm={this.state.kpm}
+      />
+       
+      )
+    }
+  }
+  
